eb77b2fde1d96e7626e6737fd3109c4e0c835c3b
Maksim VOznyarskyi <maksimv@uw.edu>
2018-08-04 00:39:53 -0700

    little program to generate a random 4 digit password

--- javaFile.java
+++ javaFile.java
@@ -1,6 +1,15 @@
-java class javaFile {
+import java.util.Random;
+public class javaFile {
 
     public static void main(String... args) {
         System.out.println("Hello, world");
+        Random r = new Random();
+
+        String result = "";
+        for (int i = 0; i < 4; i++) {
+
+            result += r.nextInt(10);
+        }
+        System.out.println("New password is " + result);
     }
 } 
